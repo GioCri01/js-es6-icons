109181690888cd76468b221d4a3eacc82f776a6d
GioCri01 <giovanni.crispino94@icloud.com>
2022-04-29 18:33:34 +0200

    milestone 2 e milestone 3

--- js/data.js
+++ js/data.js
@@ -113,21 +113,84 @@ const arrObj=[
 		}
 		];
 		
+		//Stampa arr di oggetti
 
-		arrObj.forEach((el)=>{
+		function stampa(arr) {
+			arr.forEach((el)=>{
 			
-			const output = 
-			`
+				const output = 
+				`
+				
+				<div class="my-card m-4 text-center  d-flex justify-content-center flex-column">
+				<span style="color:${el.color};"><i class="${el.prefix}solid ${el.prefix}${el.name}"></i></span>
+					<h2>${el.name}</h2>
+				</div>
+	
+				`
+				
+				
+				document.getElementById("container").innerHTML += output;
+	
+				
+				
+			})
 			
-            <div class="my-card m-4 text-center  d-flex justify-content-center flex-column">
-			<i class="${el.prefix}solid ${el.prefix}${el.name}"></i>
-                <h2>${el.name}</h2>
-            </div>
+		}
 
-			`
-			console.log(output);
-			document.getElementById("container").innerHTML += output;
+		// divido gli oggetti in diversi array in base al type
+
+		const animal = arrObj.filter((el)=>{
+			if(el.type === "animal"){
+				return true;
+			}
+			
 		})
 
+		const vegetable = arrObj.filter((el)=>{
+			if(el.type === "vegetable"){
+				return true;
+			}
+		})
 
+		const user = arrObj.filter((el)=>{
+			if(el.type === "user"){
+				return true;
+			}
+		})
+
+		console.log(animal);
+		console.log(vegetable);
+		console.log(user);
+
+		const selectBtn = document.getElementById("type")
+		selectBtn.addEventListener("change", function() {
+			if(selectBtn.value == "0")
+			{	reset();
+				stampa(arrObj)	;
+			}else if(selectBtn.value == "1"){
+				reset();
+				stampa(animal)	;
+			}else if(selectBtn.value == "2"){
+				reset();
+				stampa(vegetable)	;
+			}else if (selectBtn.value == "3"){
+				reset();
+				stampa(user);
+			}
+		});
+		
+
+		
+
+		function reset() {
+			const main = document.getElementById("container");
+			main.innerHTML = "";
+			
+		}
+
+		
+
+		
+
+		
 		
